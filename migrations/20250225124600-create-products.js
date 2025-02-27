ce1e7d93aab982183143203208290872d9cafda7
Mikin33 <68873413+Mikin33@users.noreply.github.com>
2025-02-28 03:12:08 +0530

    Update 20250225124600-create-products.js

--- migrations/20250225124600-create-products.js
+++ migrations/20250225124600-create-products.js
@@ -24,17 +24,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         defaultValue: 0,
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.fn("NOW"),
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.fn("NOW"), 
-      },
+      }
     });
   },
 
